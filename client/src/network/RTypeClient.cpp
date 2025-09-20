489ef3d5df7374e7ffd1857834868edefc25744d
Dytoma Batogouma <dytomaomar@gmail.com>
2025-09-20 18:13:22 +0200

    IMPLEMENT: functions from the abstract class and integrate windows api

--- client/src/network/RTypeClient.cpp
+++ client/src/network/RTypeClient.cpp
@@ -9,18 +9,89 @@
 
 using namespace Client;
 
+#ifdef _WIN32
+#include <windows.h>
+static std::atomic<bool> stop_requested { false };
+
+BOOL WINAPI consoleCtrlHandler(DWORD ctrlType)
+{
+    if (ctrlType == CTRL_C_EVENT) {
+        stop_requested = true;
+        std::cout << "\nStopping server gracefully..." << std::endl;
+        return TRUE;
+    }
+    return FALSE;
+}
+#else
+#include <csignal>
 static RTypeClient* instance;
 
-RTypeClient::RTypeClient(const uint16_t port)
-    : m_port(port)
+void RTypeClient::handleSignal(int)
+{
+    if (instance) {
+        instance->requestStop();
+        std::cout << "\nStopping server gracefully..." << std::endl;
+    }
+}
+#endif
+
+void RTypeClient::requestStop()
+{
+    _running = false;
+}
+
+RTypeClient::RTypeClient(const std::string& t_serverIpAddress, const uint16_t t_serverPort,
+    const uint16_t t_port)
+    : m_port(t_port)
+    , m_serverInfo { 0, t_serverIpAddress, t_serverPort }
+    , m_debug(false)
+    , m_msgSequenceNumber(0)
 {
     _socket = std::make_unique<UdpSocket>(m_port);
 }
 
+RTypeClient::RTypeClient(const uint16_t t_port)
+    : m_port(t_port)
+    , m_debug(false)
+    , m_msgSequenceNumber(0)
+{
+    m_serverInfo = { 0, "", 4242 };
+}
+
+// setters
+void RTypeClient::setServerInfo(const std::string& t_serverIpAddress, const uint16_t t_serverPort)
+{
+    m_serverInfo = { 0, t_serverIpAddress, t_serverPort };
+}
+
+void RTypeClient::setDebugMode(const bool t_debugMode)
+{
+    m_debug = t_debugMode;
+}
+
 void RTypeClient::start()
 {
+#ifdef _WIN32
+    SetConsoleCtrlHandler(consoleCtrlHandler, TRUE);
+#else
     instance = this;
     std::signal(SIGINT, SIG_DFL);
+#endif
+    while (_running) {
+#ifdef _WIN32
+        if (stop_requested) {
+            requestStop();
+            break;
+        }
+#endif
+
+        processIncomingMessages();
+        processOutgoingMessages();
+    }
+
+#ifndef _WIN32
+    instance = nullptr;
+#endif
 }
 
 void RTypeClient::stop()
@@ -32,10 +103,48 @@ void RTypeClient::stop()
     }
 }
 
-void RTypeClient::handleSignal(int)
+void RTypeClient::sendMessage(const MessageType t_type)
 {
-    if (instance) {
-        instance->requestStop();
-        std::cout << "\nStopping server gracefully..." << std::endl;
-    }
+    const Message msg(t_type, m_msgSequenceNumber, 0, 1);
+    const std::vector<uint8_t> msgData = msg.serialize();
+
+    queueMessage(msg, m_serverInfo);
+}
+
+void RTypeClient::sendMessage(const MessageType t_type, uint32_t t_payload)
+{
+    Message msg(t_type, m_msgSequenceNumber, 0, 1);
+
+    msg.write(t_payload);
+    queueMessage(msg, m_serverInfo);
+}
+
+void RTypeClient::connectToServerRequest()
+{
+    sendMessage(MessageType::CONNECT);
+}
+
+void RTypeClient::disconnectFromServerRequest()
+{
+    sendMessage(MessageType::DISCONNECT);
+}
+
+void RTypeClient::startGameRequest()
+{
+    sendMessage(MessageType::START_GAME);
+}
+
+void RTypeClient::lobbyStartRequest()
+{
+    sendMessage(MessageType::LOBBY_STATE);
+}
+
+void RTypeClient::registerHandlers()
+{
+    _handlers[MessageType::CONNECT_ACK] = [this](const Message& t_msg, PeerInfo& t_peerInfo) {
+        handleConnectionAccepted(t_msg, t_peerInfo);
+    };
+    _handlers[MessageType::LOBBY_INFO] = [this](const Message& t_msg, PeerInfo& t_peerInfo) {
+        handleLobbyJoint(t_msg, t_peerInfo);
+    };
 }
--- client/src/network/RTypeClient.hpp
+++ client/src/network/RTypeClient.hpp
@@ -8,16 +8,45 @@
 namespace Client {
 class RTypeClient : public RTypeNetwork {
 public:
-    explicit RTypeClient(uint16_t port);
+    explicit RTypeClient(uint16_t t_port);
+    RTypeClient(const std::string& t_serverIpAddress, uint16_t t_serverPort, uint16_t t_port);
 
     void start() override;
     void stop() override;
     void requestStop() override;
 
+    // setters
+
+    /*
+     * This function sets the server ip address and server port to
+     * establish connection between client and server
+     **/
+    void setServerInfo(const std::string& t_serverIpAddress, uint16_t t_serverPort);
+    void setDebugMode(bool t_debugMode);
+
     static void handleSignal(int t_signal);
+
+    // server communication functions
+    void sendMessage(MessageType t_type);
+    void sendMessage(MessageType t_type, uint32_t t_payload);
+    void connectToServerRequest();
+    void disconnectFromServerRequest();
+    void createLobbyRequest();
+    void joinLobbyRequest(uint32_t t_lobbyId);
+    void startGameRequest();
+    void lobbyStartRequest();
 
 private:
     void registerHandlers() override;
     uint16_t m_port;
+    PeerInfo m_serverInfo;
+    bool m_debug;
+    uint16_t m_msgSequenceNumber;
+    uint32_t m_lobbyId = 0;
+
+    // server responses handlers
+    void handleConnectionAccepted(const Message& t_msg, PeerInfo& t_peerInfo);
+    void handleLobbyCreation(const Message& t_msg, PeerInfo& t_peerInfo);
+    void handleLobbyJoint(const Message& t_msg, PeerInfo& t_peerInfo);
 };
 }
