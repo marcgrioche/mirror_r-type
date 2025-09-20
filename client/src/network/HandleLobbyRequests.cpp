e055ac0a9c82b37b4f2201174ab344891f55f106
Dytoma Batogouma <dytomaomar@gmail.com>
2025-09-20 18:12:35 +0200

    IMPLEMENT(HandleLobbyRequests.cpp): lobby joint and creation handlers

---
+++ client/src/network/HandleLobbyRequests.cpp
@@ -0,0 +1,38 @@
+#include "RTypeClient.hpp"
+#include <iostream>
+
+using namespace Client;
+
+// requests handlers
+void RTypeClient::createLobbyRequest()
+{
+    sendMessage(MessageType::CREATE_LOBBY);
+}
+
+void RTypeClient::joinLobbyRequest(const uint32_t t_lobbyId)
+{
+    sendMessage(MessageType::JOIN_LOBBY, t_lobbyId);
+}
+
+// responses handlers
+void RTypeClient::handleLobbyCreation(const Message& t_msg, PeerInfo& t_peerInfo)
+{
+    (void)t_peerInfo; // unused parameter
+    const uint32_t lobbyId = t_msg.readU32();
+
+    m_lobbyId = lobbyId;
+    std::cout << "Lobby " << lobbyId << " created" << std::endl;
+}
+
+void RTypeClient::handleLobbyJoint(const Message& t_msg, PeerInfo& t_peerInfo)
+{
+    (void)t_peerInfo;
+    const uint32_t lobbyId = t_msg.readU32();
+
+    if (lobbyId == 0) {
+        std::cout << "Lobby not found" << std::endl;
+    } else {
+        m_lobbyId = lobbyId;
+        std::cout << "Joined lobby " << lobbyId << std::endl;
+    }
+}
