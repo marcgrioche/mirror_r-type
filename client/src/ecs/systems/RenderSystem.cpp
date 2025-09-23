f66d07125f23743e0172763ff46e8021dc2c40ea
MacBookPro <macbookpro@macbookpro.home>
2025-09-23 16:31:24 +0200

    ADD(render): renderSystem now draw white hitboxes

--- client/src/ecs/systems/RenderSystem.cpp
+++ client/src/ecs/systems/RenderSystem.cpp
@@ -23,16 +23,14 @@ void renderSystem(Registry& registry)
     graphics.clear(20, 30, 50, 255);
 
     graphics.setDrawColor(255, 255, 255, 255);
-
-    auto view = registry.view<Position>();
-    for (auto [pos] : view) {
+    auto view = registry.view<Hitbox, Position>();
+    for (auto [hitbox, pos] : view) {
         std::cout << "Rendering entity at position (" << pos.x << ", " << pos.y << ")\n";
         SDL_Rect rect = {
             static_cast<int>(pos.x),
             static_cast<int>(pos.y),
-            15, 15
+            static_cast<int>(hitbox.width), static_cast<int>(hitbox.height)
         };
-
         SDL_RenderFillRect(renderer, &rect);
         SDL_RenderDrawRect(renderer, &rect);
     }
