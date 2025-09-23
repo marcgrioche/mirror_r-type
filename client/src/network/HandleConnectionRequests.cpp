05609e38792d219c118f38bab3f0ab8d71c85883
Dytoma Batogouma <dytomaomar@gmail.com>
2025-09-23 15:46:37 +0200

    REMOVE: unnecessary message

--- client/src/network/HandleConnectionRequests.cpp
+++ client/src/network/HandleConnectionRequests.cpp
@@ -8,7 +8,7 @@ using namespace Client;
 void RTypeClient::handleConnectionAccepted(const Message& t_msg, PeerInfo& t_peerInfo)
 {
     (void)t_msg;
-    std::cout << "Connection accepted" << std::endl;
+    m_eventsQueue.push({ MessageType::CONNECT_ACK, true });
     if (m_serverInfo.ip_address != t_peerInfo.ip_address || m_serverInfo.port != t_peerInfo.port) {
         m_serverInfo = t_peerInfo;
     }
