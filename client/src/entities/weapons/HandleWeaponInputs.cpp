43484bc993a68e74fbb076dae462ff3e590bbdc1
CB4fr2 <baptiste.camerlynck@epitech.eu>
2025-09-30 14:56:10 +0200

    ADD(weapon): projectile speed improved

--- client/src/entities/weapons/HandleWeaponInputs.cpp
+++ client/src/entities/weapons/HandleWeaponInputs.cpp
@@ -27,7 +27,7 @@ void handleWeaponInputs(InputManager& _inputs, Registry& registry)
                 auto entity = it.entity();
                 if (registry.has<Position>(parent.parent)) {
                     const Position& parentPosition = registry.get<Position>(parent.parent);
-                    factories::createProjectile(registry, Vector2{parentPosition.x, parentPosition.y}, Vector2{1, 0}, Parent{entity});
+                    factories::createProjectile(registry, Vector2{parentPosition.x, parentPosition.y}, Vector2{500, 0}, Parent{entity});
                 } else {
                     std::cout << "Parent has no Position component" << std::endl;
                 }
