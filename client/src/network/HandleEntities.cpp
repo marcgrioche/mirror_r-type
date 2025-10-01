405c6549a12a1e491207d51d4136eeb96e825036
Dytoma Batogouma <dytomaomar@gmail.com>
2025-10-01 10:04:42 +0200

    ADD(HandleEntities.cpp): function to implement the actions related to the entities

---
+++ client/src/network/HandleEntities.cpp
@@ -0,0 +1,9 @@
+#include "RTypeClient.hpp"
+
+using namespace Client;
+
+void RTypeClient::handleEntitySpawn(const Message& t_msg, PeerInfo& t_peerInfo)
+{
+    (void)t_msg;
+    (void)t_peerInfo;
+}
