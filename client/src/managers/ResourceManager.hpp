3c14468afd129486cc5080c7ac397930cedb7426
Dytoma Batogouma <dytomaomar@gmail.com>
2025-10-11 12:17:05 +0200

    ADD(ResourceManager): loading fonts at the start and reusing them

--- client/src/managers/ResourceManager.hpp
+++ client/src/managers/ResourceManager.hpp
@@ -1,21 +1,25 @@
 #pragma once
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <string>
 #include <unordered_map>
 
 class ResourceManager {
-    public:
-        static ResourceManager& getInstance();
+public:
+    static ResourceManager& getInstance();
 
-        SDL_Texture* loadTexture(SDL_Renderer* renderer, const std::string& id, const std::string& path);
-        SDL_Texture* getTexture(const std::string& id) const;
-        void unloadAll();
+    SDL_Texture* loadTexture(SDL_Renderer* renderer, const std::string& id, const std::string& path);
+    SDL_Texture* getTexture(const std::string& id) const;
+    void loadFonts(const std::string& t_fontPath, int t_fontSize);
+    TTF_Font* getFont(int t_fontSize);
+    void unloadAll();
 
-    private:
-        ResourceManager() = default;
-        ~ResourceManager();
+private:
+    ResourceManager() = default;
+    ~ResourceManager();
 
-        std::unordered_map<std::string, SDL_Texture*> textures;
+    std::unordered_map<std::string, SDL_Texture*> textures;
+    std::string defaultFontPath = "client/res/fonts/OpenSans-Medium.ttf";
+    std::unordered_map<int, TTF_Font*> fonts;
 };
-
