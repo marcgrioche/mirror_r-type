73d2d33932298647a7cc6571ff436a4e8650a1a8
Noe Roberties <noe.roberties@epitech.eu>
2025-10-13 10:36:48 +0200

    FIX(Config.hpp) fix platform velocity

--- shared/include/Config.hpp
+++ shared/include/Config.hpp
@@ -26,8 +26,12 @@ constexpr float PLATFORM_WIDTH = 120.0f;
 constexpr float PLATFORM_HEIGHT = 20.0f;
 constexpr float PLATFORM_LEVELS[] = { 200.0f, 300.0f, 400.0f, 500.0f };
 constexpr int PLATFORM_LEVELS_COUNT = 4;
-constexpr int PLATFORM_VELOCITY_X = -15.0f;
+constexpr int PLATFORM_VELOCITY_X = -30.0f;
 constexpr int PLATFORM_VELOCITY_Y = 0.0f;
+constexpr float PLATFORM_MIN_DIST_X = 200.0f;
+constexpr float PLATFORM_MAX_DIST_X = 270.0f;
+constexpr float PLATFORM_CROSS_DIST_X = PLATFORM_WIDTH * 1.5f;
+constexpr float PLATFORM_MAX_DIFF_Y = 100.0f;
 
 // PowerUp system constants
 constexpr float POWERUP_WIDTH = 20.0f;
@@ -54,7 +58,3 @@ constexpr float PROJECTILE_HEIGHT = 25.0f;
 constexpr float PROJECTILE_DAMAGE = 5.0f;
 constexpr float PROJECTILE_LIFETIME = 30.0f;
 
-constexpr float PLATFORM_MIN_DIST_X = 200.0f;
-constexpr float PLATFORM_MAX_DIST_X = 270.0f;
-constexpr float PLATFORM_CROSS_DIST_X = PLATFORM_WIDTH * 1.5f;
-constexpr float PLATFORM_MAX_DIFF_Y = 100.0f;
