dccde90ae1bbd4061351c3cae90a3c8a1e679378
Dytoma Batogouma <dytomaomar@gmail.com>
2025-10-15 22:41:35 +0200

    EXTRACT(DashSystem): changes of components from entities to implement it for prediction

--- shared/src/ecs/systems/DashSystem.cpp
+++ shared/src/ecs/systems/DashSystem.cpp
@@ -6,30 +6,36 @@
 */
 
 #include "DashSystem.hpp"
-#include "components/Dash.hpp"
-#include "components/Velocity.hpp"
 #include "Dash.hpp"
 #include "FrequencyUtils.hpp"
 #include <iostream>
-#include "components/RigidBody.hpp"
+
+void changeDashComponentProperties(
+    Dash& t_dash,
+    Velocity& t_velocity,
+    RigidBody& t_rigidBody,
+    const float t_deltaTime)
+{
+    if (t_dash.isDashing) {
+        t_dash.remaining -= t_deltaTime;
+
+        t_velocity.v.x = t_dash.dashSpeed * t_dash.direction.x;
+        t_velocity.v.y = t_dash.dashSpeed * t_dash.direction.y;
+
+        if (t_dash.remaining <= 0.0f) {
+            t_rigidBody.active = true; // reactivate rigid body
+            t_dash.isDashing = false;
+            t_dash.remaining = 0.0f;
+            FrequencyUtils::reset(t_dash.cooldown);
+        }
+    }
+}
 
 void dashSystem(Registry& registry, float deltaTime)
 {
     auto view = registry.view<Dash, Velocity, RigidBody>();
 
     for (auto [dash, velocity, rigidBody] : view) {
-        if (dash.isDashing) {
-            dash.remaining -= deltaTime;
-
-            velocity.v.x = dash.dashSpeed * dash.direction.x;
-            velocity.v.y = dash.dashSpeed * dash.direction.y;
-
-            if (dash.remaining <= 0.0f) {
-                rigidBody.active = true; //reactivate rigid body
-                dash.isDashing = false;
-                dash.remaining = 0.0f;
-                FrequencyUtils::reset(dash.cooldown);
-            }
-        }
+        changeDashComponentProperties(dash, velocity, rigidBody, deltaTime);
     }
 }
