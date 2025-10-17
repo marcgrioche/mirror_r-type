54ca1560a13c648dbd5198789e7be57c8282068a
Dytoma Batogouma <dytomaomar@gmail.com>
2025-10-17 13:00:29 +0200

    FIX(FunctionMotionUtils.hpp): compilation error due to missing include file and order of structure initialization

--- shared/src/ecs/components/componentutils/FunctionMotionUtils.hpp
+++ shared/src/ecs/components/componentutils/FunctionMotionUtils.hpp
@@ -8,6 +8,7 @@
 #pragma once
 #include "FunctionMotion.hpp"
 #include "Position.hpp"
+#include <cmath>
 
 FunctionMotion getFunctionMotionSineAbsolute(Position origin, float vx = 300.f, float frequency = 3.f, float amplitude = 100.f)
 {
@@ -16,10 +17,10 @@ FunctionMotion getFunctionMotionSineAbsolute(Position origin, float vx = 300.f, 
             (void)dt;
             float x = vx * t;
             float y = std::sin(t * frequency * 2.f * M_PI) * amplitude;
-            return Vector2{x, y};
+            return Vector2 { x, y };
         },
-        .origin = origin,
-        .mode = MotionMode::Absolute
+        .mode = MotionMode::Absolute,
+        .origin = origin
     };
     return fm;
 }
@@ -29,7 +30,7 @@ FunctionMotion getFunctionMotionSineRelative(float vx = 400.f, float frequency =
     FunctionMotion fm = {
         .function = [vx, frequency, amplitude](float t, float dt) {
             float vy = std::cos(t * frequency * 2.f * M_PI) * amplitude * frequency * 2.f * M_PI;
-            return Vector2{vx * dt, vy * dt};
+            return Vector2 { vx * dt, vy * dt };
         },
         .mode = MotionMode::Relative
     };
