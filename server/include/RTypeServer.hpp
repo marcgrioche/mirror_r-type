892c0996e142d5bb6b90915176639f0fff9dc3b1
Dytoma Batogouma <dytomaomar@gmail.com>
2025-10-20 12:30:42 +0200

    IMPLEMENT(RTypeServer): function to kick players out of lobby

--- server/include/RTypeServer.hpp
+++ server/include/RTypeServer.hpp
@@ -69,6 +69,8 @@ private:
     void handleStartGame(const Message& msg, PeerInfo& peerInfo);
     void handleLobbyState(const Message& msg, PeerInfo& peerInfo);
     void handleUsername(const Message& msg, PeerInfo& peerInfo);
+    void handleKickPlayer(const Message& msg, PeerInfo& peerInfo);
+    void removePlayerAndNotifyLobby(const Message& msg, const Lobby* t_lobby, uint32_t playerId);
     // ...other handlers per message type
 
     // utility methods
